f3631f8aca64ac33b4f28b2488bf7d27099fcf4e
yanyang.wang <yanyang.wang@ocado.com>
2018-12-31 15:07:31 +0000

    optmize code

--- src/main/scala/string/LongestCommonSearch.scala
+++ src/main/scala/string/LongestCommonSearch.scala
@@ -2,101 +2,120 @@ package string
 
 import scala.annotation.tailrec
 
-/**
-  * Author: yanyang.wang
-  * Date: 13/12/2018
-  */
-object BoyerMooreSearch {
+object LongestCommonSearch {
   def main(args: Array[String]): Unit = {
-    test(bruteForce)
-    test(bmSearch)
+    println("==> " + search("ABCBX", "ABDCAB") + " <==")
+    println("==> " + search("ABCBX", "KLMK") + " <==")
+    println("==> " + search("XMJYAUZ", "MZJAWXU") + " <==")
+    println("==> " + search("Hello World", "Bonjour le monde") + " <==")
+
   }
 
-  private def test(search: (String, String) => List[Int]): Unit = {
-    val target = "Hello, World"
-    val pattern = "Wor"
-    println(search(target, pattern))
-    println(search(target, pattern.head.toString))
-    println(search(target, "oel"))
-    println(search(target, "l"))
-    println(search(target, "ll"))
-    println(search(target, "w"))
-    println(search(target, ""))
-    println(search("", pattern))
-    println(search("w", pattern))
-    println(search(
-      "ACCCGGTTTTAAAGAACCACCATAAGATATAGACAGATATAGGACAGATATAGAGACAAAACCCCATACCCCAATATTTTTTTGGGGAGAAAAACACCACAGATAGATACACAGACTACACGAGATACGACATACAGCAGCATAACGACAACAGCAGATAGACGATCATAACAGCAATCAGACCGAGCGCAGCAGCTTTTAAGCACCAGCCCCACAAAAAACGACAATFATCATCATATACAGACGACGACACGACATATCACACGACAGCATA",
-      "CATA"
-    )) // [20, 64, 130, 140, 166, 234, 255, 270]
-  }
+  def search(s1: String, s2: String): String = {
+    val matrix = buildMatrix(s1, s2)
 
-  def bruteForce(target: String, pattern: String): List[Int] = {
-    def isEnd(s: String, index: Int): Boolean = index == s.length - 1
+    def backTrack(matrix: Matrix[Int]): String = {
 
-    def matchString(targetIndex: Int): Boolean = {
-      (0 until pattern.length)
-        .zip(targetIndex until target.length)
-        .takeWhile { case (pIndex, tIndex) =>
-          pattern.charAt(pIndex) == target.charAt(tIndex)
-        }
-        .lastOption
-        .exists(_._1 == pattern.length - 1)
-    }
-
-    @tailrec
-    def searchRec(result: List[Int],
-                  currentTargetIndex: Int): List[Int] = {
-      if (currentTargetIndex + pattern.length > target.length) {
-        result
-      } else {
-        if (matchString(currentTargetIndex)) {
-          searchRec(result :+ currentTargetIndex, currentTargetIndex + 1)
-        } else {
-          searchRec(result, currentTargetIndex + 1)
-        }
-      }
-    }
-
-    if (target.isEmpty || pattern.isEmpty) Nil
-    else {
-      searchRec(Nil, 0)
-    }
-  }
-
-  def bmSearch(target: String, pattern: String): List[Int] = {
-    if (target.isEmpty || pattern.isEmpty || pattern.length > target.length) Nil
-    else {
-      val patternLength = pattern.length
-      val skipTable: Map[Char, Int] =
-        pattern.zipWithIndex.map { case (c, i) => c -> {
-          val skip = patternLength - i - 1
-          if (skip == 0) patternLength else skip
-        }}.toMap
-
-      def computeRightmostIndex(currentIndex: Int): Int =
-        currentIndex + skipTable.getOrElse(target.charAt(currentIndex), patternLength)
-
-      def matchString(targetIndex: Int): Option[Int] = {
-        (patternLength - 1 to 0 by -1)
-          .zip(targetIndex to 0 by -1)
-          .takeWhile { case (pIndex, tIndex) =>
-            pattern.charAt(pIndex) == target.charAt(tIndex)
-          }
-          .lastOption
-          .filter(_._1 == 0)
-          .map(_._2)
+      def updateResult(r: Vector[Char], row: Int, current: Int, target: Int): Vector[Char] = {
+        if (current != target) r :+ s1.charAt(row) else r
       }
 
       @tailrec
-      def rec2(result: List[Int], currentRightmostIndex: Int): List[Int] = {
-        if (currentRightmostIndex >= target.length) result
+      def rec(result: Vector[Char], r: Int, c: Int): Vector[Char] = {
+        val current = matrix.get(r, c)
+        val updator: Int => Vector[Char] = updateResult(result, r, current, _)
+        if (r == 0 && c == 0) updator(0)
         else {
-          val newRightmostIndex = computeRightmostIndex(currentRightmostIndex)
-          rec2(result ++ matchString(currentRightmostIndex), newRightmostIndex)
+          val up = matrix.safeGet(r - 1, c)
+          val left = matrix.safeGet(r, c - 1)
+          val upLeft = matrix.safeGet(r - 1, c - 1)
+
+          (upLeft, up, left) match {
+            case (None, None, Some(l)) => rec(updator(l), r, c - 1)
+            case (None, Some(u), None) => rec(updator(u), r - 1, c)
+            case (Some(ul), Some(u), Some(l)) =>
+              val max = List(ul, u, l).max
+              val (newR, newC) = if (ul == max) (r - 1, c - 1)
+              else if (u == max) (r - 1, c)
+              else (r, c - 1)
+              rec(updator(max), newR, newC)
+            case error => throw new IllegalStateException(s"unexpected state $error")
+          }
         }
       }
 
-      rec2(Nil, patternLength - 1)
+      rec(Vector.empty, s1.length - 1, s2.length - 1).reverse.mkString("")
     }
+
+    backTrack(matrix)
   }
+
+  private def buildMatrix(s1: String, s2: String): Matrix[Int] = {
+    def computeValue(ri: Int, ci: Int, matrix: Matrix[Int]): Int = {
+      if (s1.charAt(ri) == s2.charAt(ci)) matrix.safeGet(ri - 1, ci - 1).getOrElse(0) + 1
+      else Math.max(matrix.safeGet(ri - 1, ci).getOrElse(0), matrix.safeGet(ri, ci - 1).getOrElse(0))
+    }
+
+    // recursion way
+    @tailrec
+    def build(startR: Int, startC: Int, matrix: Matrix[Int]): Matrix[Int] = {
+      if (startR >= s1.length) matrix
+      else {
+        val updatedMatrix = (startC until s2.length).foldLeft(matrix) {
+          (result, i) =>
+            val newValue = computeValue(startR, i, result)
+            result.update(startR, i, newValue)
+        }
+        build(startR + 1, startC, updatedMatrix)
+      }
+    }
+
+    val rawMatrix = Matrix.init(s1.length, s2.length, 0)
+    val result = (0 until s1.length).foldLeft(rawMatrix) { (m, r) =>
+      (0 until s2.length).foldLeft(m) {
+        (result, i) =>
+          val newValue = computeValue(r, i, result)
+          result.update(r, i, newValue)
+      }
+    }
+
+    result.prettyPrint(s1, s2) // debug purpose
+    result
+  }
+
+}
+
+
+case class Matrix[T](private val matrix: Vector[Vector[T]]) {
+  def get(r: Int, c: Int): T = matrix(r)(c)
+
+  private def safeGet[O](v: Vector[O], i: Int): Option[O] = if (i >= 0 && i < v.length) Some(v(i)) else None
+
+  def safeGet(r: Int, c: Int): Option[T] = safeGet(matrix, r).flatMap(row => safeGet(row, c))
+
+  def update(r: Int, c: Int, value: T): Matrix[T] = {
+    val newRow = matrix(r).updated(c, value)
+    Matrix(matrix.updated(r, newRow))
+  }
+
+  def prettyPrint(rowHead: String, columnHead: String): Unit =
+    prettyPrint(convert(rowHead), convert(columnHead))
+
+  private def convert(s: String): Vector[String] = s.toCharArray.map(_.toString).toVector
+
+  def prettyPrint(rowHead: Vector[String], columnHead: Vector[String]): Unit = {
+    val stringMatrix = matrix.map(_.map(_.toString))
+    val table = (columnHead +: stringMatrix).zipWithIndex.map { case (a, i) =>
+      val head = if (i == 0) '+' else rowHead(i - 1)
+      (head +: a).mkString("\t")
+    }.mkString("\n")
+    println(table)
+  }
+}
+
+object Matrix {
+  def init[T](row: Int, column: Int, default: T): Matrix[T] =
+    Matrix(Vector.fill(row)(Vector.fill(column)(default)))
+
+  def fromVector[T](matrix: Vector[Vector[T]]): Matrix[T] = Matrix(matrix)
 }

100f5b33b890fafa607ee4a7880d0c59666b6b78
yanyang.wang <yanyang.wang@ocado.com>
2019-01-29 11:01:54 +0000

    Add Pick and test case for Generator

--- src/main/scala/book/gen/Generator.scala
+++ src/main/scala/book/gen/Generator.scala
@@ -1,14 +1,41 @@
 package book.gen
 
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
 /**
   * Author: yanyang.wang
   * Date: 21/10/2018
   */
 object Generator {
   def main(args: Array[String]): Unit = {
-    val one = rollDie.run(SimpleRNG(5))._1
-    println(one)
+    /*val one = rollDie.run(SimpleRNG(5))._1
+    println(one)*/
+    test()
   }
+
+  private def test(): Unit = {
+    val rand = both(
+      pickAndExclude((1 to 50).toVector, 5),
+      pickAndExclude((1 to 12).toVector, 2)
+    )
+    val seedString: String = {
+      val format = new SimpleDateFormat("yyyyMM")
+      val result = "testSample" + format.format(Calendar.getInstance().getTime)
+      println(result)
+      result
+    }
+
+    val seed = BigInt(seedString.toCharArray.map(_.toInt).mkString("")).longValue()
+
+    println(seed)
+    val result = rand.run(SimpleRNG(seed))._1
+    val result2 = rand.run(SimpleRNG(seed))._1
+    require(result == result2)
+    println(result)
+  }
+
+
 
   type Rand[A] = State[RNG, A]
   val int: Rand[Int] = State(_.nextInt)
@@ -19,10 +46,26 @@ object Generator {
 
   def nonNegativeEven: Rand[Int] = nonNegativeInt.map(i => i - i % 2)
 
+  def choose(start: Int, stopExclusive: Int): Rand[Int] = nonNegativeInt.map(n => start + n % (stopExclusive - start))
+
+  def pick(ints: Vector[Int]): Rand[Int] = choose(0, ints.size).map(ints)
+
+  def pickAndExclude(ints: Vector[Int], count: Int): Rand[List[Int]] = {
+    require(count <= ints.size)
+    State { rng =>
+      val result = List.fill(count)(pick(_)).foldLeft((List.empty[Int], rng, ints)) { (acc, picker) =>
+        val (r, state, poll) = acc
+        val (int, newState) = picker(poll).run(state)
+        val newPoll = poll.filterNot(_ == int)
+        (r :+ int, newState, newPoll)
+      }
+      (result._1, result._2)
+    }
+  }
+
   val boolean: Rand[Boolean] = int.map(_ % 2 == 0)
   val intInt: Rand[(Int, Int)] = both(int, int)
   val double: Rand[Double] = nonNegativeInt.map(_.toDouble / (Int.MaxValue + 1))
-
 
   val intDouble: Rand[(Int, Double)] = both(int, double)
 
